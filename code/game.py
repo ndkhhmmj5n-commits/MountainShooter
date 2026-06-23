e8e7ddbbaf9633352a15386c57c0df2943fd43e6
KarenSawada <karensawada@icloud.com>
2026-06-23 22:36:25 +0900

    Creating menu background image and music

--- code/game.py
+++ code/game.py
@@ -3,14 +3,19 @@
 
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size = (600, 480))
+        self.window = pygame.display.set_mode(size = (WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+
+
         while True:
             menu = Menu(self.window)
             menu.run()
